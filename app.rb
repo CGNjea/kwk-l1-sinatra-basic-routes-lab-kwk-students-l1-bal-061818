5fcfb29afa03c693cb2175973514dcdf8fc9e285
CGNjea <aryeejodi@gmail.com>
2018-06-26 18:55:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end 
+  get '/' do 
+    "My hometown is Bronx,NY"
+  end
+  get '/' do 
 end
